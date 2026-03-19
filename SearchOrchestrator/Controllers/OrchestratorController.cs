81c5f46336417923badde68e25ff3178d37316da
Dmitriy Sannikov <omeom@mail.ru>
2026-03-19 19:02:09 +0400

    feat: реализованы контроллеры

---
+++ SearchOrchestrator/Controllers/OrchestratorController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SearchOrchestrator.Application.DTO;
+using SearchOrchestrator.Application.Interfaces;
+
+namespace SearchOrchestrator.Controllers
+{
+
+    [ApiController]
+    [Route("api")]
+    public class OrchestratorController : ControllerBase
+    {
+        private readonly IIndexingService _indexingService;
+
+        public OrchestratorController(IIndexingService indexingService)
+        {
+            _indexingService = indexingService;
+        }
+
+        [HttpGet("tasks/{taskId}")]
+        public async Task<IActionResult> GetStatus(Guid taskId)
+        {
+            var result = await _indexingService.GetStatusAsync(taskId);
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpGet("search")]
+        public async Task<IActionResult> Search([FromQuery] string query)
+        {
+            var result = await _indexingService.SearchAsync(query);
+            return Ok(result);
+        }
+
+        [HttpPost("index")]
+        public async Task <IActionResult> Start([FromBody] StartIndexingRequestDto requestDto)
+        {
+            var result = await _indexingService.StartAsync(requestDto);
+            return Ok(result);
+        }
+    }
+}
--- SearchOrchestrator/Program.cs
+++ SearchOrchestrator/Program.cs
@@ -1,12 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using SearchOrchestrator.Application.Interfaces;
+using SearchOrchestrator.Application.Services;
+using SearchOrchestrator.Infrastructure.Persistence;
+using SearchOrchestrator.Infrastructure.Search;
+using SearchOrchestrator.Infrastructure.Workers;
+using Serilog;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseInMemoryDatabase("orchestrator-db"));
+
+// TODO вынести DI
+builder.Services.AddScoped<IIndexingService, IndexingService>();
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+builder.Services.AddSingleton<ISearchEngineClient, FakeSearchEngineClient>();
+
+builder.Services.AddHostedService<IndexingWorker>();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
